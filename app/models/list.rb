5788ddeabb1833ad6cd85fa982875c02f9c37729
andrea-marketing <andrea.marketing.kedge@gmail.com>
2022-07-06 12:10:48 +0200

    practice

---
+++ app/models/list.rb
@@ -0,0 +1,9 @@
+class List < ApplicationRecord
+  has_many :bookmarks
+  has_many :movies, through: :bookmarks, dependent: :destroy
+  has_one_attached :photo
+
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+puts 'deleting database'
+
+List.destroy_all
+Movie.destroy_all
+
+puts 'creating movies'
+
+# TODO - Let's fetch name and bio from a given GitHub username
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+
+movie_serialized = URI.open(url).read
+movies = JSON.parse(movie_serialized)
+
+results = movies['results']
+poster_url = 'https://image.tmdb.org/t/p/w500/'
+
+results.each do |movie|
+  Movie.create(title: movie['original_title'],
+               overview: movie['overview'],
+               poster_url: poster_url + movie['poster_path'],
+               rating: movie['vote_average'])
+end
+
+puts 'done'
